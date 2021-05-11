7e2ba5941277fb7cd19676ee0221aa73199532b2
pedro-ddias <69222299+pedro-ddias@users.noreply.github.com>
2021-05-10 22:46:50 -0300

    API de CEP

--- codigo/FutManager.Sistema.Frontend/js/cadastroUsuario.js
+++ codigo/FutManager.Sistema.Frontend/js/cadastroUsuario.js
@@ -1,3 +1,22 @@
+'use strict'
+
+const preencherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+const pesquisarCep = async () => {
+    const cep = document.getElementById('cep').value;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+    preencherFormulario(endereco);
+    console.log(endereco);
+}
+
+document.getElementById('cep').addEventListener('focusout', pesquisarCep)
 
 //validação de campos vazios
 function validar() {
--- codigo/FutManager.Sistema.Frontend/js/cadastroUsuarioComplemento.js
+++ codigo/FutManager.Sistema.Frontend/js/cadastroUsuarioComplemento.js
@@ -8,15 +8,6 @@ function validar() {
         return false;
     }
 
-    //verificar e-mail são iguais
-    if (confirmEmail.value == email.value) {
-        return true;
-    } else {
-        alert("E-mails são diferentes")
-        confirmEmail.focus();
-        return false;
-    }
-
     //verificar se confirmar email está vazio
     var confirmEmail = document.getElementById("confirmEmail");
     if (confirmEmail.value == "") {
@@ -24,7 +15,6 @@ function validar() {
         confirmEmail.focus();
         return false;
     }
-
 
     //verificar se senha está vazio
     var senha = document.getElementById("senha");
@@ -43,5 +33,13 @@ function validar() {
         return false;
     }
 
+    //verificar e-mail são iguais
+    if (confirmEmail.value == email.value) {
+        return true;
+    } else {
+        alert("E-mails são diferentes")
+        confirmEmail.focus();
+        return false;
+    }
 
 }
