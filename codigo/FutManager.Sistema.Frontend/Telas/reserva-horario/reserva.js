35aa7f02d20171c179a3a70121919dcc9b2792fd
= <=>
2021-05-16 17:12:58 -0300

    Add. evolução de front e back para processo de reserva

--- codigo/FutManager.Sistema.Frontend/Telas/reserva-horario/reserva.js
+++ codigo/FutManager.Sistema.Frontend/Telas/reserva-horario/reserva.js
@@ -2,26 +2,89 @@ $(document).ready(function () {
     $("#container-resultado").hide();
 
     $("#btn-buscar").click(function () {
-        // let horarioList = $.ajax( "endpoint", {param: 'bla, bla, bla'});
+        const filtro = {
+            cidade: $("#input-cidade").val(),
+            estado: $("#input-estado").val(),
+            horario_inicio: $("#timerpicker-horario-inicio").val(),
+            horario_final: $("#timepicker-horario-final").val(),
+            data_inicio: $("#datepicker-data-inicio").val(),
+            data_final: $("#datepicker-data-final").val()
+        };
 
-        // if (any) // dropdown nenhum registro encontrado
+        validar_filtro(filtro);
 
-        // horarioList.forEach(horario => {
-        //     $('#here_table table').append( '<tr><td>' + horario + '</td></tr>' );
-        // });
-
-        // $("#container-resultado").show();
+        $.get("http://localhost:3000/reservas/horariosDisponiveis", filtro, callback_busca);
     });
 
     $("#btn-limpar").click(function () {
-        // limpar campos
-        $.getJSON("http://localhost:3000/boletos", function(result){
-            $("#container-resultado").show();
-        });
+        $("#input-cidade").val();
+        $("#input-estado").val();
+        $("#timerpicker-horario-inicio").val('');
+        $("#timepicker-horario-final").val('');
+        $("#datepicker-data-inicio").val('');
+        $("#datepicker-data-final").val('');
+        $("table tr").remove();
+        $("#container-resultado").hide();
     });
 
     $("#btn-confirmar-reserva").click(function () {
+        $("table tr input:checkbox[name=reservar]:checked")
+            .each((index, checkbox) => {
+                let params = $(checkbox).val().split('|');
+                let data = {
+                    dia: params[1],
+                    horario: params[0],
+                    confirmada: 0
+                };
+
+                $.ajax({
+                    url: "http://localhost:3000/reservas",
+                    type: "POST",
+                    dataType: "json",
+                    contentType: "application/json",
+                    data: JSON.stringify(data),
+                    success: function (result) {
+                        alert("Pré-reserva realizada com sucesso!")
+                    },
+                    error: function (err) {
+                        console.log(err);
+                    }
+                });
+            });
+
+
         // apresentar modal com os horários selecionados
         // usar spinner
     });
 });
+
+function callback_busca(horarios, status) {
+    if (status === "error") {
+        alert("Algo inesperado ocorreu durante a busca por horários!");
+        return;
+    }
+
+    montar_tabela(horarios);
+};
+
+function montar_tabela(horarios) {
+    let rows = '';
+
+    horarios.forEach(horario => {
+        rows +=
+            `<tr>
+            <td><input type="checkbox" name="reservar" value="${horario.id_horario}|${horario.dia_disponivel}"></td>
+            <td>${horario.nome_quadra}</td>
+            <td>${horario.dia_disponivel}</td>
+            <td>${horario.horario_inicio}</td>
+            <td>${horario.horario_final}</td>
+        </tr>`;
+    });
+
+    $('#table-horarios-disponiveis').append(rows);
+    $("#container-resultado").show();
+};
+
+function validar_filtro(filtro) {
+
+};
--- codigo/fut-manager-strapi/api/reserva/controllers/reserva.js
+++ codigo/fut-manager-strapi/api/reserva/controllers/reserva.js
@@ -1,8 +1,58 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
 
-module.exports = {};
+    async horariosDisponiveis(ctx) {
+        let filtro = ctx.query;
+
+        const knex = strapi.connections.default;
+        const horarios = await knex('quadras')
+            .join('enderecos', 'quadras.endereco', 'enderecos.id')
+            .join('horarios', 'horarios.quadra', 'quadras.id')
+            .where('enderecos.estado', filtro.estado)
+            .where('enderecos.cidade', filtro.cidade)
+            .select('enderecos.logradouro')
+            .select('horarios.id AS id_horario')
+            .select('horarios.dia_abertura')
+            .select('horarios.horario_inicio')
+            .select('horarios.horario_final')
+            .select('quadras.id AS id_quadra')
+            .select('quadras.razao_social');
+
+        let data_inicio = new Date(filtro.data_inicio);
+        let data_final = new Date(filtro.data_final);
+
+        console.log(filtro);
+
+        let agendas = [];
+        // Pecorre todas as dias compreendidas entre a data de início e fim
+        // gerando, em memória, as agendas. Certamente não é a melhor solução.
+        while (data_inicio <= data_final) {
+            for (let index = 0; index < horarios.length; index++) {
+                // Horário disponível é para o mesmo dia da semana do filtro?
+                if (data_inicio.getDay() == horarios[index].dia_abertura) {
+                    // Horário disponível compreende o horário do filtro?
+                    if (filtro.horario_inicio <= horarios[index].horario_inicio
+                        && filtro.horario_final >= horarios[index].horario_final) {
+                        agendas.push(
+                            {
+                                dia_disponivel: data_inicio,
+                                id_quadra: horarios[index].id_quadra,
+                                nome_quadra: horarios[index].razao_social,
+                                id_horario: horarios[index].id_horario,
+                                horario_inicio: horarios[index].horario_inicio,
+                                horario_final: horarios[index].horario_final
+                            }
+                        );
+                    }
+                }
+            }
+            // Incrementa em um dia a data de início
+            data_inicio.setDate(data_inicio.getDate() + 1);
+        }
+        console.log(data_inicio);
+        console.log(data_final);
+
+        return ctx.send(agendas);
+    },
+};
