075eee26cffc0ffa26bc39c518d6d3ea6e06fa67
Octávio Martins <oapmartins@sga.pucminas.br>
2021-05-20 21:12:23 -0300

    Alterção

--- codigo/FutManager.Sistema.Backend.Api/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'cc3cc56e2f02be8a1871e0bb9a0551dd'),
-    },
-  },
-});
--- codigo/FutManager.Sistema.Backend.Api/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '25bb4c8b-0cad-4232-9b4d-49a762382726'
-};
